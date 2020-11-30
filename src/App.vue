70395577041dacd869ee2e8fbc7a42a9d66f670e
kazushi-takagaki <tkgkkzs@gmail.com>
2020-11-30 15:01:05 +0900

    complete this course

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,196 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <button @click="myAnimation = 'slide'">Slide</button>
+    <button @click="myAnimation = 'fade'">Fade</button>
+    <p>{{ myAnimation }}</p>
+    <br />
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin; auto;">
+      <transition-group name="fade">
+        <li
+          v-for="(number, index) in numbers"
+          @click="remove(index)"
+          :key="number"
+        >
+          {{ number }}
+        </li>
+      </transition-group>
+    </ul>
+    <br />
+    <button @click="show = !show">切り替え</button>
+    <br /><br />
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
+
+    <br />
+    <button @click="myComponent = 'ComponentA'">ComponentA</button>
+    <button @click="myComponent = 'ComponentB'">ComponentB</button>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
+    <transition name="fade" mode="in-out">
+      <p v-if="show">sayonara</p>
+      <div v-if="!show">konnnitiha</div>
+    </transition>
+    <transition
+      name="fade"
+      enter-active-class="animate__animated animate__bounce"
+      leave-active-class="animate__animated animate__swing"
+      appear
+    >
+      <p v-if="show">Hello</p>
+    </transition>
+    <transition :name="myAnimation" appear>
+      <p v-show="show">bye!</p>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ComponentA,
+    ComponentB,
+  },
+  data() {
+    return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
+      show: true,
+      myAnimation: "slide",
+      myComponent: "ComponentA",
+    };
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      // 現れる前
+      el.style.transform = "scale(0)";
+    },
+    enter(el, done) {
+      // 現れるとき
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1;
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter() {
+      // 現れた後
+    },
+    entercancelled() {
+      // 現れるアニメーションがキャンセルされたとき
+    },
+    beforeLeave() {
+      // 消える前
+    },
+    leave(el, done) {
+      // 消えるとき
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1;
+        if (scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave() {
+      // 消えた後
+    },
+    leavecancelled() {
+      // 消えるアニメーションがキャンセルされたとき
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 50%;
+  background-color: deeppink;
+}
+
+.fade-enter {
+  /* 現れるときの最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れるときのトランジションの状態 */
+  transition: opacity 2s;
+}
+.fade-enter-to {
+  /* 現れるときの最後の状態 */
+  opacity: 1;
+}
+.fade-leave {
+  /* 消えるときの最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消えるときのトランジションの状態 */
+  transition: opacity 2s;
+}
+.fade-leave-to {
+  /* 消えるときの最後の状態 */
+  opacity: 0;
+}
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 0.5s;
+  transition: opacity 1s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+  transition: opacity 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+.main {
+  width: 70%;
+  padding-top: 5rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
